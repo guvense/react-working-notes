622663c375d4324c48cbcd543f9e19e914c9b7e1
guvense <guven.seckin.4@gmail.com>
2019-09-25 21:33:18 +0300

    Adding a dummy project

--- indecision-app/public/scripts/app.js
+++ indecision-app/public/scripts/app.js
@@ -9,185 +9,264 @@ function _possibleConstructorReturn(self, call) { if (!self) { throw new Referen
 function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function, not " + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
 
 var Indesicion = function (_React$Component) {
-    _inherits(Indesicion, _React$Component);
+  _inherits(Indesicion, _React$Component);
 
-    function Indesicion(props) {
-        _classCallCheck(this, Indesicion);
+  function Indesicion(props) {
+    _classCallCheck(this, Indesicion);
 
-        var _this = _possibleConstructorReturn(this, (Indesicion.__proto__ || Object.getPrototypeOf(Indesicion)).call(this, props));
+    var _this = _possibleConstructorReturn(this, (Indesicion.__proto__ || Object.getPrototypeOf(Indesicion)).call(this, props));
 
-        _this.deleteAllTask = _this.deleteAllTask.bind(_this);
-        _this.addTask = _this.addTask.bind(_this);
-        _this.state = {
-            taskList: ['first']
+    _this.deleteAllTask = _this.deleteAllTask.bind(_this);
+    _this.addTask = _this.addTask.bind(_this);
+    _this.state = {
+      taskList: []
+    };
+    return _this;
+  }
+
+  _createClass(Indesicion, [{
+    key: "deleteAllTask",
+    value: function deleteAllTask() {
+      this.setState(function () {
+        return {
+          taskList: []
         };
-        return _this;
+      });
     }
+  }, {
+    key: "addTask",
+    value: function addTask(value) {
+      this.setState(function (preventDefault) {
+        return {
+          taskList: preventDefault.taskList.concat(value)
+        };
+      });
+    }
+  }, {
+    key: "render",
+    value: function render() {
+      return React.createElement(
+        "div",
+        null,
+        React.createElement(Header, null),
+        React.createElement(Information, null),
+        React.createElement(UserInfo, { username: "guven" }),
+        React.createElement(AddDummyTask, { handlerDummyTaskAdders: this.addTask }),
+        React.createElement(TaskList, { taskList: this.state.taskList }),
+        React.createElement(AddTask, { handlerTaskAdding: this.addTask }),
+        React.createElement(DeleteAllTask, { handlerDeleteTask: this.deleteAllTask })
+      );
+    }
+  }]);
 
-    _createClass(Indesicion, [{
-        key: "deleteAllTask",
-        value: function deleteAllTask() {
-            this.setState(function () {
-                return {
-                    taskList: []
-                };
-            });
-        }
-    }, {
-        key: "addTask",
-        value: function addTask(value) {
-            this.setState(function (preventDefault) {
-
-                return {
-                    taskList: preventDefault.taskList.concat(value)
-                };
-            });
-        }
-    }, {
-        key: "render",
-        value: function render() {
-            return React.createElement(
-                "div",
-                null,
-                React.createElement(Header, null),
-                React.createElement(TaskList, { taskList: this.state.taskList }),
-                React.createElement(AddTask, { handlerTaskAdding: this.addTask }),
-                React.createElement(DeleteAllTask, { handlerDeleteTask: this.deleteAllTask })
-            );
-        }
-    }]);
-
-    return Indesicion;
+  return Indesicion;
 }(React.Component);
 
 var Header = function (_React$Component2) {
-    _inherits(Header, _React$Component2);
+  _inherits(Header, _React$Component2);
 
-    function Header() {
-        _classCallCheck(this, Header);
+  function Header() {
+    _classCallCheck(this, Header);
 
-        return _possibleConstructorReturn(this, (Header.__proto__ || Object.getPrototypeOf(Header)).apply(this, arguments));
+    return _possibleConstructorReturn(this, (Header.__proto__ || Object.getPrototypeOf(Header)).apply(this, arguments));
+  }
+
+  _createClass(Header, [{
+    key: "render",
+    value: function render() {
+      return React.createElement(
+        "h1",
+        null,
+        "Welcome to task management"
+      );
     }
+  }]);
 
-    _createClass(Header, [{
-        key: "render",
-        value: function render() {
-
-            return React.createElement(
-                "h1",
-                null,
-                "Welcome to task management"
-            );
-        }
-    }]);
-
-    return Header;
+  return Header;
 }(React.Component);
 
-var TaskList = function (_React$Component3) {
-    _inherits(TaskList, _React$Component3);
+// const Information = (props) => {
+//   return (<p>App Owner: {props.ownerName}</p>);
+// };
 
-    function TaskList(props) {
-        _classCallCheck(this, TaskList);
+var Information = function (_React$Component3) {
+  _inherits(Information, _React$Component3);
 
-        return _possibleConstructorReturn(this, (TaskList.__proto__ || Object.getPrototypeOf(TaskList)).call(this, props));
+  function Information() {
+    _classCallCheck(this, Information);
+
+    return _possibleConstructorReturn(this, (Information.__proto__ || Object.getPrototypeOf(Information)).apply(this, arguments));
+  }
+
+  _createClass(Information, [{
+    key: "render",
+    value: function render() {
+      return React.createElement(
+        "p",
+        null,
+        "App Owner: ",
+        this.props.ownerName
+      );
     }
+  }]);
 
-    _createClass(TaskList, [{
-        key: "render",
-        value: function render() {
-            return this.props.taskList.map(function (o) {
-                return React.createElement(Task, { value: o });
-            });
-        }
-    }]);
-
-    return TaskList;
+  return Information;
 }(React.Component);
 
-var Task = function (_React$Component4) {
-    _inherits(Task, _React$Component4);
+Information.defaultProps = {
+  ownerName: "Kral Guven"
+};
 
-    function Task(props) {
-        _classCallCheck(this, Task);
+var TaskList = function (_React$Component4) {
+  _inherits(TaskList, _React$Component4);
 
-        return _possibleConstructorReturn(this, (Task.__proto__ || Object.getPrototypeOf(Task)).call(this, props));
+  function TaskList(props) {
+    _classCallCheck(this, TaskList);
+
+    return _possibleConstructorReturn(this, (TaskList.__proto__ || Object.getPrototypeOf(TaskList)).call(this, props));
+  }
+
+  _createClass(TaskList, [{
+    key: "render",
+    value: function render() {
+      return this.props.taskList.map(function (o) {
+        return React.createElement(Task, { value: o });
+      });
     }
+  }]);
 
-    _createClass(Task, [{
-        key: "render",
-        value: function render() {
-            return React.createElement(
-                "p",
-                null,
-                this.props.value
-            );
-        }
-    }]);
-
-    return Task;
+  return TaskList;
 }(React.Component);
 
-var AddTask = function (_React$Component5) {
-    _inherits(AddTask, _React$Component5);
+var Task = function (_React$Component5) {
+  _inherits(Task, _React$Component5);
 
-    function AddTask(props) {
-        _classCallCheck(this, AddTask);
+  function Task(props) {
+    _classCallCheck(this, Task);
 
-        var _this5 = _possibleConstructorReturn(this, (AddTask.__proto__ || Object.getPrototypeOf(AddTask)).call(this, props));
+    return _possibleConstructorReturn(this, (Task.__proto__ || Object.getPrototypeOf(Task)).call(this, props));
+  }
 
-        _this5.addTask = _this5.addTask.bind(_this5);
-        return _this5;
+  _createClass(Task, [{
+    key: "render",
+    value: function render() {
+      return React.createElement(
+        "p",
+        null,
+        this.props.value
+      );
     }
+  }]);
 
-    _createClass(AddTask, [{
-        key: "addTask",
-        value: function addTask(e) {
-            e.preventDefault();
-            var val = e.target.elements.taskName.value;
-
-            this.props.handlerTaskAdding(val);
-        }
-    }, {
-        key: "render",
-        value: function render() {
-            return React.createElement(
-                "form",
-                { onSubmit: this.addTask },
-                React.createElement("input", { type: "text", name: "taskName" }),
-                React.createElement(
-                    "button",
-                    null,
-                    "Add task"
-                )
-            );
-        }
-    }]);
-
-    return AddTask;
+  return Task;
 }(React.Component);
 
-var DeleteAllTask = function (_React$Component6) {
-    _inherits(DeleteAllTask, _React$Component6);
+var AddTask = function (_React$Component6) {
+  _inherits(AddTask, _React$Component6);
 
-    function DeleteAllTask() {
-        _classCallCheck(this, DeleteAllTask);
+  function AddTask(props) {
+    _classCallCheck(this, AddTask);
 
-        return _possibleConstructorReturn(this, (DeleteAllTask.__proto__ || Object.getPrototypeOf(DeleteAllTask)).apply(this, arguments));
+    var _this6 = _possibleConstructorReturn(this, (AddTask.__proto__ || Object.getPrototypeOf(AddTask)).call(this, props));
+
+    _this6.addTask = _this6.addTask.bind(_this6);
+    return _this6;
+  }
+
+  _createClass(AddTask, [{
+    key: "addTask",
+    value: function addTask(e) {
+      e.preventDefault();
+      var val = e.target.elements.taskName.value;
+      e.target.elements.taskName.value = "";
+
+      this.props.handlerTaskAdding(val);
     }
+  }, {
+    key: "render",
+    value: function render() {
+      return React.createElement(
+        "form",
+        { onSubmit: this.addTask },
+        React.createElement("input", { type: "text", name: "taskName" }),
+        React.createElement(
+          "button",
+          null,
+          "Add task"
+        )
+      );
+    }
+  }]);
 
-    _createClass(DeleteAllTask, [{
-        key: "render",
-        value: function render() {
-            return React.createElement(
-                "button",
-                { onClick: this.props.handlerDeleteTask },
-                "Delete All Tasks"
-            );
-        }
-    }]);
-
-    return DeleteAllTask;
+  return AddTask;
 }(React.Component);
 
-ReactDOM.render(React.createElement(Indesicion, null), document.getElementById('apptext'));
+var DeleteAllTask = function (_React$Component7) {
+  _inherits(DeleteAllTask, _React$Component7);
+
+  function DeleteAllTask() {
+    _classCallCheck(this, DeleteAllTask);
+
+    return _possibleConstructorReturn(this, (DeleteAllTask.__proto__ || Object.getPrototypeOf(DeleteAllTask)).apply(this, arguments));
+  }
+
+  _createClass(DeleteAllTask, [{
+    key: "render",
+    value: function render() {
+      return React.createElement(
+        "button",
+        { onClick: this.props.handlerDeleteTask },
+        "Delete All Tasks"
+      );
+    }
+  }]);
+
+  return DeleteAllTask;
+}(React.Component);
+
+var AddDummyTask = function (_React$Component8) {
+  _inherits(AddDummyTask, _React$Component8);
+
+  function AddDummyTask(props) {
+    _classCallCheck(this, AddDummyTask);
+
+    // this.addDummyTasks=this.addDummyTasks.bind(this)
+    var _this8 = _possibleConstructorReturn(this, (AddDummyTask.__proto__ || Object.getPrototypeOf(AddDummyTask)).call(this, props));
+
+    _this8.addDummyTasks = function () {
+      for (var index = 0; index < 10; index++) {
+
+        _this8.props.handlerDummyTaskAdders(index);
+      }
+    };
+
+    return _this8;
+  }
+
+  _createClass(AddDummyTask, [{
+    key: "render",
+    value: function render() {
+      return React.createElement(
+        "button",
+        { onClick: this.addDummyTasks },
+        "Add Dummy"
+      );
+    }
+  }]);
+
+  return AddDummyTask;
+}(React.Component);
+
+var UserInfo = function UserInfo(props) {
+  return React.createElement(
+    "div",
+    null,
+    React.createElement(
+      "p",
+      null,
+      "Username: ",
+      props.username
+    )
+  );
+};
+ReactDOM.render(React.createElement(Indesicion, null), document.getElementById("apptext"));
--- indecision-app/src/playground/indesicion_app.js
+++ indecision-app/src/playground/indesicion_app.js
@@ -1,130 +1,140 @@
-
-
 class Indesicion extends React.Component {
-    constructor(props){
-        super(props)
-        this.deleteAllTask=this.deleteAllTask.bind(this)
-        this.addTask=this.addTask.bind(this)
-        this.state= {
-            taskList: ['first']
-        }
-    }
+  constructor(props) {
+    super(props);
+    this.deleteAllTask = this.deleteAllTask.bind(this);
+    this.addTask = this.addTask.bind(this);
+    this.state = {
+      taskList: []
+    };
+  }
 
-    deleteAllTask(){
-        this.setState(()=>{
-            return{
-            taskList: []
-            }
-        })
-    }
+  deleteAllTask() {
+    this.setState(() => {
+      return {
+        taskList: []
+      };
+    });
+  }
 
-    addTask(value){
-        this.setState((preventDefault)=>{
-          
-            return {
-                taskList: preventDefault.taskList.concat(value)
-            }
-        })
-    }
+  addTask(value) {
+    this.setState(preventDefault => {
+      return {
+        taskList: preventDefault.taskList.concat(value)
+      };
+    });
+  }
 
-
-    render(){
-        return(
-            <div>
-            <Header/>
-            <TaskList taskList={this.state.taskList}/>
-            <AddTask handlerTaskAdding={this.addTask} />
-            <DeleteAllTask handlerDeleteTask={this.deleteAllTask}/>
-            </div>
-        )
-
-    }
+  render() {
+    return (
+      <div>
+        <Header />
+        <Information />
+        <UserInfo username="guven" />
+        <AddDummyTask handlerDummyTaskAdders={this.addTask} />
+        <TaskList taskList={this.state.taskList} />
+        <AddTask handlerTaskAdding={this.addTask} />
+        <DeleteAllTask handlerDeleteTask={this.deleteAllTask} />
+      </div>
+    );
+  }
 }
-
 
 class Header extends React.Component {
-    render(){
-
-        return(
-            <h1>Welcome to task management</h1>
-        )
-    }
+  render() {
+    return <h1>Welcome to task management</h1>;
+  }
 }
 
+// const Information = (props) => {
+//   return (<p>App Owner: {props.ownerName}</p>);
+// };
+
+class Information extends React.Component {
+  render() {
+    return <p>App Owner: {this.props.ownerName}</p>;
+  }
+}
+
+Information.defaultProps = {
+  ownerName: "Kral Guven"
+};
+
 class TaskList extends React.Component {
-    constructor(props){
-        super(props)
+  constructor(props) {
+    super(props);
+  }
 
-    }
-
-    render(){
-        return(
-            this.props.taskList.map((o)=><Task value={o}/>)
-        )
-    }
-
-
-
+  render() {
+    return this.props.taskList.map(o => <Task value={o} />);
+  }
 }
 
 class Task extends React.Component {
+  constructor(props) {
+    super(props);
+  }
 
-    constructor(props){
-        super(props)
-
-    }
-
-    render(){
-        return(
-            <p>{this.props.value}</p>
-        )
-    }
+  render() {
+    return <p>{this.props.value}</p>;
+  }
 }
-
-
 
 class AddTask extends React.Component {
-    constructor(props){
-        super(props)
-        this.addTask=this.addTask.bind(this)
-    }
+  constructor(props) {
+    super(props);
+    this.addTask = this.addTask.bind(this);
+  }
 
+  addTask(e) {
+    e.preventDefault();
+    const val = e.target.elements.taskName.value;
+    e.target.elements.taskName.value = "";
 
+    this.props.handlerTaskAdding(val);
+  }
 
-    addTask(e){
-        e.preventDefault();
-        const val= e.target.elements.taskName.value
-
-        this.props.handlerTaskAdding(val)
-        
-        
-
-    }
-
-
-    render(){
-        return(
-            <form onSubmit= {this.addTask}>
-            <input type="text" name= "taskName"/>
-            <button>Add task</button>
-            
-            </form>
-        )
-    }
-
+  render() {
+    return (
+      <form onSubmit={this.addTask}>
+        <input type="text" name="taskName" />
+        <button>Add task</button>
+      </form>
+    );
+  }
 }
-
 
 class DeleteAllTask extends React.Component {
-
-
-render(){
-    return(
-        <button onClick= {this.props.handlerDeleteTask}>Delete All Tasks</button>
-    )
+  render() {
+    return (
+      <button onClick={this.props.handlerDeleteTask}>Delete All Tasks</button>
+    );
+  }
 }
 
+class AddDummyTask extends React.Component {
+  constructor(props) {
+    super(props);
+   // this.addDummyTasks=this.addDummyTasks.bind(this)
+    this.addDummyTasks= () => {
+        for (let index = 0; index < 10; index++) {
+    
+          this.props.handlerDummyTaskAdders(index);
+        }
+  }
 
+
+  };
+
+  render() {
+    return <button onClick={this.addDummyTasks}>Add Dummy</button>;
+  }
 }
 
-ReactDOM.render(<Indesicion />, document.getElementById('apptext'))
+const UserInfo = props => {
+  return (
+    <div>
+      <p>Username: {props.username}</p>
+    </div>
+  );
+};
+ReactDOM.render(<Indesicion />, document.getElementById("apptext"));
